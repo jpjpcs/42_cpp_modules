a1be304e12137b6134509a51c722fdc6a4a7b4a9
jpjpcs <joaopaulojesuspereiracosilva@gmail.com>
2024-09-17 16:46:25 +0100

    megaphone {} updated

--- CPP00_CPP04/CPP00/ex00/megaphone.cpp
+++ CPP00_CPP04/CPP00/ex00/megaphone.cpp
@@ -6,21 +6,23 @@
 /*   By: joaosilva <joaosilva@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 15:27:18 by joaosilva         #+#    #+#             */
-/*   Updated: 2024/09/16 16:27:54 by joaosilva        ###   ########.fr       */
+/*   Updated: 2024/09/17 16:45:33 by joaosilva        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-void printUppercase(const char* str) {
+void printUppercase(const char* str) 
+{
     while (*str) {
         std::cout << (char)std::toupper(*str);
         ++str;
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     if (argc > 1) {
         for (int i = 1; i < argc; ++i) {
             printUppercase(argv[i]);
